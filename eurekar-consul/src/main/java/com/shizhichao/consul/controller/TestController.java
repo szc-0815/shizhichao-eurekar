9111812830b66909b31aa311780532fb669fbc57
szc-0815 <shizhichao5@outlook.com>
2020-04-20 20:21:52 +0800

    consul的上传

---
+++ eurekar-consul/src/main/java/com/shizhichao/consul/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.shizhichao.consul.controller;
+
+import com.shizhichao.consul.config.ConsulConfigInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Autowired
+    private ConsulConfigInfo consulConfigInfo;
+
+
+    @RequestMapping("getConfig")
+    public Object getConfig() {
+
+        //return configInfo;
+        return consulConfigInfo;
+    }
+}
